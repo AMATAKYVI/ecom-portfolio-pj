b1de40e1be287a54ef13d1691d078daf37a7b026
AMATAKYVI <yviamatak123@gmail.com>
2024-05-03 00:11:14 -0400

    refacter banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,31 +5,39 @@ interface BannerProps {}
 
 const Banner: FC<BannerProps> = ({}) => {
   return (
-    <div className="flex relative h-[500px] bg-gray-100 p-10 justify-center">
-      <div className="flex flex-col justify-start gap-10 py-10 px-10">
-        <h1 className="text-5xl font-bold">Welcome to our store</h1>
-        <p className="text-gray-900">Get the best shoes at the best prices</p>
-        <div className="flex gap-5 justify-start">
-          <button className="">
-            <span>Shop Now</span>
-          </button>
-          <button>
-            <span>Learn More</span>
-          </button>
+    <>
+      <div className="mx-auto w-[100vw] sm:w-[95vw] md:w-[90vw] lg:w-[85vw] flex justify-center xs:flex-col py-10 px-5  bg-amber-300 relative  rounded shadow-sm">
+        <div className="flex flex-col justify-center gap-5 ">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+            Welcome to our store
+          </h1>
+          <p className="text-sm sm:text-sm md:text-md lg:text-lg text-gray-900">
+            Get the best shoes at the best prices
+          </p>
+          <div className="text-sm sm:text-sm md:text-md lg:text-lg  flex gap-5 ">
+            <button className="">
+              <span>Shop Now</span>
+            </button>
+            <button>
+              <span>Learn More</span>
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-center items-center relative">
+          {/* some style float */}
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="w-[250px] h-[250px] opacity-30 rounded-full bg-orange-400 blur z-0"></div>
+          </div>
+          <Image
+            src="/banner.png"
+            alt="shoes"
+            width="300"
+            height="300"
+            className="z-10"
+          />
         </div>
       </div>
-      <Image
-        src="/banner.png"
-        alt="shoes"
-        width="300"
-        height="300"
-        className="absolute top-1/2 left-[220px] transform -translate-x-1/2 -translate-y-1/2 z-[1]"
-      />
-      <div className="z-[2] text-black bg-gray-200 bg-opacity-200 ">
-        something here maybe some catchy text dont overcomplicate this it just
-        for fun
-      </div>
-    </div>
+    </>
   );
 };
 
